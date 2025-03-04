b15ba5ad08cee9a01e776fa4c6537438e9bbb1b6
Red <johnny.komng@gmail.com>
2025-03-04 10:18:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+
+import os
 import discord
 from discord.ext import commands
+from myserver import server_on
 
 bot = commands.bot(command_prefix='!', intents=discord.Intents.all())
 
+@bot.event
+async def on_ready():
+    print("Bot Online!")
+
+server_on()
+
+bot.run(os.getenv('TOKEN'))
